e8b8692e001b5cea6438c656ef974dd5af805552
unknown <pchoi13370@gmail.com>
2020-08-08 23:55:40 -0700

    inital commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,29 +21,38 @@ const Hero = styled("div")`
        margin-bottom: 3em;
     }
 
-    h1 {
-        margin-bottom: 1em;
-
+    h1:nth-of-type(1) {
         a {
-            text-decoration: none;
-            transition: all 100ms ease-in-out;
-
             &:nth-of-type(1) { color: ${colors.blue500}; }
-            &:nth-of-type(2) { color: ${colors.orange500}; }
-            &:nth-of-type(3) { color: ${colors.purple500}; }
-            &:nth-of-type(4) { color: ${colors.green500}; }
-            &:nth-of-type(5) { color: ${colors.teal500}; }
 
             &:hover {
-                cursor: pointer;
-                transition: all 100ms ease-in-out;
 
                 &:nth-of-type(1) { color: ${colors.blue600};    background-color: ${colors.blue200};}
-                &:nth-of-type(2) { color: ${colors.orange600};  background-color: ${colors.orange200};}
-                &:nth-of-type(3) { color: ${colors.purple600};  background-color: ${colors.purple200};}
-                &:nth-of-type(4) { color: ${colors.green600};   background-color: ${colors.green200};}
-                &:nth-of-type(5) { color: ${colors.teal600};    background-color: ${colors.teal200};}
+            }
+        }
+    }
 
+
+    h1:nth-of-type(2) {
+        a {
+            &:nth-of-type(1) { color: ${colors.orange500}; }
+            &:nth-of-type(2) { color: ${colors.purple500}; }
+            &:nth-of-type(3) { color: ${colors.green500}; }
+
+            &:hover {
+                &:nth-of-type(1) { color: ${colors.orange600};  background-color: ${colors.orange200};}
+                &:nth-of-type(2) { color: ${colors.purple600};  background-color: ${colors.purple200};}
+                &:nth-of-type(3) { color: ${colors.green600};   background-color: ${colors.green200};}
+            }
+        }
+    }
+
+    h1:nth-of-type(3) {
+        a {
+            &:nth-of-type(1) { color: ${colors.teal500}; }
+
+            &:hover {
+                &:nth-of-type(1) { color: ${colors.teal600};    background-color: ${colors.teal200};}
             }
         }
     }
