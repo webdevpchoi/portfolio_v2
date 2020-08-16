3a2ed3531a6741f8325535e457ea0022dd6452d3
webdevpchoi <webdevpchoi@gmail.com>
2020-08-15 21:24:05 -0700

    added links to projects

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,124 +1,129 @@
-import React from "react";
-import Button from "components/_ui/Button";
-import styled from "@emotion/styled";
-import dimensions from "styles/dimensions";
-import { RichText } from "prismic-reactjs";
-import PropTypes from "prop-types";
+import React from "react"
+import Button from "components/_ui/Button"
+import styled from "@emotion/styled"
+import dimensions from "styles/dimensions"
+import { RichText } from "prismic-reactjs"
+import PropTypes from "prop-types"
 
 const AboutContainer = styled("div")`
-    padding-top: 1em;
-    display: grid;
-    grid-template-columns: 8em 1fr 8em;
-    grid-gap: 3em;
+  padding-top: 1em;
+  display: grid;
+  grid-template-columns: 8em 1fr 8em;
+  grid-gap: 3em;
 
-    @media(max-width: ${dimensions.maxwidthTablet}px) {
-        grid-template-columns: 1fr 3fr 1fr;
-    }
+  @media (max-width: ${dimensions.maxwidthTablet}px) {
+    grid-template-columns: 1fr 3fr 1fr;
+  }
 
-    @media(max-width: ${dimensions.maxwidthMobile}px) {
-        grid-template-columns: 7em 1fr;
-        grid-template-rows: 3em 1fr;
-        grid-gap: 2em;
-    }
+  @media (max-width: ${dimensions.maxwidthMobile}px) {
+    grid-template-columns: 7em 1fr;
+    grid-template-rows: 3em 1fr;
+    grid-gap: 2em;
+  }
 `
 
 const AboutLinkContainer = styled("div")`
-    padding-top: 1em;
-    padding-bottom: 3em;
-    display: flex;
-    flex-direction: column;
+  padding-top: 1em;
+  padding-bottom: 3em;
+  display: flex;
+  flex-direction: column;
 
-    @media(max-width: ${dimensions.maxwidthMobile}px) {
-        grid-row: 2;
-    }
+  @media (max-width: ${dimensions.maxwidthMobile}px) {
+    grid-row: 2;
+  }
 `
 
 const AboutLink = styled("a")`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 1.5em;
-    font-weight: 600;
-    line-height: 1.9;
-    text-decoration: none;
-    color: currentColor;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 1.5em;
+  font-weight: 600;
+  line-height: 1.9;
+  text-decoration: none;
+  color: currentColor;
 
-    .link-icon {
-        width: 25px;
-        height: 25px;
-        margin-right: .5rem;
-    }
+  .link-icon {
+    width: 25px;
+    height: 25px;
+    margin-right: 0.5rem;
+  }
 
-    span:not(.link-text) {
-        margin-left: 1em;
-        transform: translateX(-8px);
-        display: inline-block;
-        opacity: 0;
-        transition: all 400ms ease-in-out;
-    }
+  span:not(.link-text) {
+    margin-left: 1em;
+    transform: translateX(-8px);
+    display: inline-block;
+    opacity: 0;
+    transition: all 400ms ease-in-out;
+  }
 
-    &:hover {
-        span {
-            transform: translateX(0px);
-            opacity: 1;
-            transition: all 150ms ease-in-out;
-        }
+  &:hover {
+    span {
+      transform: translateX(0px);
+      opacity: 1;
+      transition: all 150ms ease-in-out;
     }
+  }
 `
 
 const AboutBio = styled("div")`
-    padding-bottom: 3em;
-    max-width: 480px;
+  padding-bottom: 3em;
+  max-width: 480px;
 
-
-    @media(max-width: ${dimensions.maxwidthMobile}px) {
-        grid-row: 2;
-    }
+  @media (max-width: ${dimensions.maxwidthMobile}px) {
+    grid-row: 2;
+  }
 `
 
 const AboutActions = styled("div")`
-    padding-top: 1em;
-    padding-bottom: 3em;
+  padding-top: 1em;
+  padding-bottom: 3em;
 
-
-    @media(max-width: ${dimensions.maxwidthMobile}px) {
-        padding: 0;
-        grid-column: 1 / -1;
-        grid-row: 1;
-    }
+  @media (max-width: ${dimensions.maxwidthMobile}px) {
+    padding: 0;
+    grid-column: 1 / -1;
+    grid-row: 1;
+  }
 `
 
-
 const About = ({ bio, socialLinks }) => (
-    <AboutContainer>
-        <AboutLinkContainer>
-            {socialLinks.map((social, i) => (
-                <AboutLink
-                    key={i}
-                    href={social.about_link[0].spans[0].data.url}
-                    target="_blank" rel="noopener noreferrer">
-                    {social.link_icon && <img className='link-icon' src={social.link_icon.url} alt={`${social.about_link[0].text} Icon `} />}
-                    <span className='link-text'>{social.about_link[0].text}</span>
-                    <span>&#8594;</span>
-                </AboutLink>
-            ))}
-        </AboutLinkContainer>
-        <AboutBio>
-            {RichText.render(bio)}
-        </AboutBio>
-        <AboutActions>
-            <a href="mailto:marguerite.roth@gmail.com" target="_blank" rel="noopener noreferrer">
-                <Button className="Button--secondary">
-                    Email me
-                </Button>
-            </a>
-        </AboutActions>
-    </AboutContainer>
+  <AboutContainer>
+    <AboutLinkContainer>
+      {socialLinks.map((social, i) => (
+        <AboutLink
+          key={i}
+          href={social.about_link[0].spans[0].data.url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {social.link_icon && (
+            <img
+              className="link-icon"
+              src={social.link_icon.url}
+              alt={`${social.about_link[0].text} Icon `}
+            />
+          )}
+          <span className="link-text">{social.about_link[0].text}</span>
+          <span>&#8594;</span>
+        </AboutLink>
+      ))}
+    </AboutLinkContainer>
+    <AboutBio>{RichText.render(bio)}</AboutBio>
+    <AboutActions>
+      <a
+        href="mailto:webdevpchoi@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Button className="Button--secondary">Email me</Button>
+      </a>
+    </AboutActions>
+  </AboutContainer>
 )
 
-export default About;
+export default About
 
 About.propTypes = {
-    bio: PropTypes.array.isRequired,
-    socialLinks: PropTypes.array.isRequired,
-};
+  bio: PropTypes.array.isRequired,
+  socialLinks: PropTypes.array.isRequired,
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,256 +1,279 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Helmet from "react-helmet";
-import { RichText } from "prismic-reactjs";
-import { graphql, Link } from "gatsby";
-import styled from "@emotion/styled";
-import colors from "styles/colors";
-import dimensions from "styles/dimensions";
-import Button from "components/_ui/Button";
-import About from "components/About";
-import Layout from "components/Layout";
-import ProjectCard from "components/ProjectCard";
-import resume from '../../Phillip-Choi-Resume.pdf';
+import React from "react"
+import PropTypes from "prop-types"
+import Helmet from "react-helmet"
+import { RichText } from "prismic-reactjs"
+import { graphql, Link } from "gatsby"
+import styled from "@emotion/styled"
+import colors from "styles/colors"
+import dimensions from "styles/dimensions"
+import Button from "components/_ui/Button"
+import About from "components/About"
+import Layout from "components/Layout"
+import ProjectCard from "components/ProjectCard"
+import resume from "../../Phillip-Choi-Resume.pdf"
 
 const Hero = styled("div")`
-    padding-top: 2.5em;
-    padding-bottom: 3em;
-    margin-bottom: 6em;
-    max-width: 830px;
+  padding-top: 2.5em;
+  padding-bottom: 3em;
+  margin-bottom: 6em;
+  max-width: 830px;
 
-    @media(max-width:${dimensions.maxwidthMobile}px) {
-       margin-bottom: 3em;
-    }
+  @media (max-width: ${dimensions.maxwidthMobile}px) {
+    margin-bottom: 3em;
+  }
 
-    h1:nth-of-type(1) {
-        a {
-            &:nth-of-type(1) { color: ${colors.blue500}; }
+  h1:nth-of-type(1) {
+    a {
+      &:nth-of-type(1) {
+        color: ${colors.blue500};
+      }
 
-            &:hover {
-
-                &:nth-of-type(1) { color: ${colors.blue600};    background-color: ${colors.blue200};}
-            }
+      &:hover {
+        &:nth-of-type(1) {
+          color: ${colors.blue600};
+          background-color: ${colors.blue200};
         }
+      }
     }
+  }
 
+  h1:nth-of-type(2) {
+    a {
+      &:nth-of-type(1) {
+        color: ${colors.orange500};
+      }
+      &:nth-of-type(2) {
+        color: ${colors.purple500};
+      }
+      &:nth-of-type(3) {
+        color: ${colors.green500};
+      }
 
-    h1:nth-of-type(2) {
-        a {
-            &:nth-of-type(1) { color: ${colors.orange500}; }
-            &:nth-of-type(2) { color: ${colors.purple500}; }
-            &:nth-of-type(3) { color: ${colors.green500}; }
-
-            &:hover {
-                &:nth-of-type(1) { color: ${colors.orange600};  background-color: ${colors.orange200};}
-                &:nth-of-type(2) { color: ${colors.purple600};  background-color: ${colors.purple200};}
-                &:nth-of-type(3) { color: ${colors.green600};   background-color: ${colors.green200};}
-            }
+      &:hover {
+        &:nth-of-type(1) {
+          color: ${colors.orange600};
+          background-color: ${colors.orange200};
         }
-    }
-
-    h1:nth-of-type(3) {
-        a {
-            &:nth-of-type(1) { color: ${colors.teal500}; }
-
-            &:hover {
-                &:nth-of-type(1) { color: ${colors.teal600};    background-color: ${colors.teal200};}
-            }
+        &:nth-of-type(2) {
+          color: ${colors.purple600};
+          background-color: ${colors.purple200};
         }
+        &:nth-of-type(3) {
+          color: ${colors.green600};
+          background-color: ${colors.green200};
+        }
+      }
     }
+  }
+
+  h1:nth-of-type(3) {
+    a {
+      &:nth-of-type(1) {
+        color: ${colors.teal500};
+      }
+
+      &:hover {
+        &:nth-of-type(1) {
+          color: ${colors.teal600};
+          background-color: ${colors.teal200};
+        }
+      }
+    }
+  }
 `
 
 const Section = styled("div")`
-    margin-bottom: 10em;
-    display: flex;
-    flex-direction: column;
+  margin-bottom: 10em;
+  display: flex;
+  flex-direction: column;
 
-    @media(max-width:${dimensions.maxwidthTablet}px) {
-        margin-bottom: 4em;
-    }
+  @media (max-width: ${dimensions.maxwidthTablet}px) {
+    margin-bottom: 4em;
+  }
 
-    &:last-of-type {
-        margin-bottom: 0;
-    }
+  &:last-of-type {
+    margin-bottom: 0;
+  }
 `
 
 const WorkAction = styled(Link)`
-    font-weight: 600;
-    text-decoration: none;
-    color: currentColor;
-    transition: all 150ms ease-in-out;
-    margin-left: auto;
+  font-weight: 600;
+  text-decoration: none;
+  color: currentColor;
+  transition: all 150ms ease-in-out;
+  margin-left: auto;
 
-    @media(max-width:${dimensions.maxwidthTablet}px) {
-       margin: 0 auto;
-    }
+  @media (max-width: ${dimensions.maxwidthTablet}px) {
+    margin: 0 auto;
+  }
+
+  span {
+    margin-left: 1em;
+    transform: translateX(-8px);
+    display: inline-block;
+    transition: transform 400ms ease-in-out;
+  }
+
+  &:hover {
+    color: ${colors.blue500};
+    transition: all 150ms ease-in-out;
 
     span {
-        margin-left: 1em;
-        transform: translateX(-8px);
-        display: inline-block;
-        transition: transform 400ms ease-in-out;
+      transform: translateX(0px);
+      opacity: 1;
+      transition: transform 150ms ease-in-out;
     }
-
-    &:hover {
-        color: ${colors.blue500};
-        transition: all 150ms ease-in-out;
-
-        span {
-            transform: translateX(0px);
-            opacity: 1;
-            transition: transform 150ms ease-in-out;
-        }
-    }
+  }
 `
 
 const RenderBody = ({ home, projects, meta }) => (
-    <>
-        <Helmet
-            title={meta.title}
-            titleTemplate={`%s | ${meta.title}`}
-            meta={[
-                {
-                    name: `description`,
-                    content: meta.description,
-                },
-                {
-                    property: `og:title`,
-                    content: meta.title,
-                },
-                {
-                    property: `og:description`,
-                    content: meta.description,
-                },
-                {
-                    property: `og:type`,
-                    content: `website`,
-                },
-                {
-                    name: `twitter:card`,
-                    content: `summary`,
-                },
-                {
-                    name: `twitter:creator`,
-                    content: meta.author,
-                },
-                {
-                    name: `twitter:title`,
-                    content: meta.title,
-                },
-                {
-                    name: `twitter:description`,
-                    content: meta.description,
-                },
-            ].concat(meta)}
+  <>
+    <Helmet
+      title={meta.title}
+      titleTemplate={`%s | ${meta.title}`}
+      meta={[
+        {
+          name: `description`,
+          content: meta.description,
+        },
+        {
+          property: `og:title`,
+          content: meta.title,
+        },
+        {
+          property: `og:description`,
+          content: meta.description,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary`,
+        },
+        {
+          name: `twitter:creator`,
+          content: meta.author,
+        },
+        {
+          name: `twitter:title`,
+          content: meta.title,
+        },
+        {
+          name: `twitter:description`,
+          content: meta.description,
+        },
+      ].concat(meta)}
+    />
+    <Hero>
+      <>{RichText.render(home.hero_title)}</>
+      <a href={resume} target="_blank" download rel="noopener noreferrer">
+        <Button>{RichText.render(home.hero_button_text)}</Button>
+      </a>
+    </Hero>
+    <Section>
+      <h1>Clients I've worked with...</h1>
+      {projects.map((project, i) => (
+        <ProjectCard
+          key={i}
+          category={project.node.project_category}
+          title={project.node.project_title}
+          description={project.node.project_preview_description}
+          thumbnail={project.node.project_preview_thumbnail}
+          uid={project.node._meta.uid}
+          link={
+            project.node.project_link ? project.node.project_link.url : null
+          }
         />
-        <Hero>
-            <>
-                {RichText.render(home.hero_title)}
-            </>
-            <a href={resume}
-               target="_blank" download rel="noopener noreferrer">
-                <Button>
-                    {RichText.render(home.hero_button_text)}
-                </Button>
-            </a>
-        </Hero>
-        <Section>
-            <h1>Clients I've worked with...</h1>
-            {projects.map((project, i) => (
-                <ProjectCard
-                    key={i}
-                    category={project.node.project_category}
-                    title={project.node.project_title}
-                    description={project.node.project_preview_description}
-                    thumbnail={project.node.project_preview_thumbnail}
-                    uid={project.node._meta.uid}
-                />
-            ))}
-            <WorkAction to={"/work"}>
-                See more! <span>&#8594;</span>
-            </WorkAction>
-        </Section>
-        <Section>
-            {RichText.render(home.about_title)}
-            <About
-                bio={home.about_bio}
-                socialLinks={home.about_links}
-            />
-        </Section>
-    </>
-);
+      ))}
+      <WorkAction to={"/work"}>
+        See more! <span>&#8594;</span>
+      </WorkAction>
+    </Section>
+    <Section>
+      {RichText.render(home.about_title)}
+      <About bio={home.about_bio} socialLinks={home.about_links} />
+    </Section>
+  </>
+)
 
 export default ({ data }) => {
-    //Required check for no data being returned
-    const doc = data.prismic.allHomepages.edges.slice(0, 1).pop();
-    const projects = data.prismic.allProjects.edges;
-    const meta = data.site.siteMetadata;
+  //Required check for no data being returned
+  const doc = data.prismic.allHomepages.edges.slice(0, 1).pop()
+  const projects = data.prismic.allProjects.edges
+  const meta = data.site.siteMetadata
 
-    if (!doc || !projects) return null;
+  if (!doc || !projects) return null
 
-    return (
-        <Layout>
-            <RenderBody home={doc.node} projects={projects} meta={meta}/>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <RenderBody home={doc.node} projects={projects} meta={meta} />
+    </Layout>
+  )
 }
 
 RenderBody.propTypes = {
-    home: PropTypes.object.isRequired,
-    projects: PropTypes.array.isRequired,
-    meta: PropTypes.object.isRequired,
-};
+  home: PropTypes.object.isRequired,
+  projects: PropTypes.array.isRequired,
+  meta: PropTypes.object.isRequired,
+}
 
 export const query = graphql`
-    {
-        prismic {
-            allHomepages {
-                edges {
-                    node {
-                        hero_title
-                        hero_button_text
-                        hero_button_link {
-                            ... on PRISMIC__ExternalLink {
-                                _linkType
-                                url
-                            }
-                        }
-                        content
-                        about_title
-                        about_bio
-                        about_links {
-                            about_link
-                            link_icon {
-                              ... on PRISMIC__ImageLink {
-                                _linkType
-                                url
-                              }
-                            }
-                        }
-                    }
+  {
+    prismic {
+      allHomepages {
+        edges {
+          node {
+            hero_title
+            hero_button_text
+            hero_button_link {
+              ... on PRISMIC__ExternalLink {
+                _linkType
+                url
+              }
+            }
+            content
+            about_title
+            about_bio
+            about_links {
+              about_link
+              link_icon {
+                ... on PRISMIC__ImageLink {
+                  _linkType
+                  url
                 }
+              }
             }
-            allProjects {
-                edges {
-                    node {
-                        project_title
-                        project_preview_description
-                        project_preview_thumbnail
-                        project_category
-                        project_post_date
-                        _meta {
-                            uid
-                        }
-                    }
-                }
-            }
+          }
         }
-        site {
-            siteMetadata {
-                title
-                description
-                author
+      }
+      allProjects {
+        edges {
+          node {
+            project_title
+            project_preview_description
+            project_preview_thumbnail
+            project_category
+            project_post_date
+            project_link {
+              ... on PRISMIC__ExternalLink {
+                url
+              }
             }
+            _meta {
+              uid
+            }
+          }
         }
+      }
     }
+    site {
+      siteMetadata {
+        title
+        description
+        author
+      }
+    }
+  }
 `
